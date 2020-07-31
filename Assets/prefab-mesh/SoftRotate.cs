6b94c30bac2bab7f406261965c4f8c7bfd9e058c
Benjamin Williams <trewelb@gmail.com>
2020-07-31 15:48:11 +0100

    added basic 3d object rendering scene

---
+++ Assets/prefab-mesh/SoftRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoftRotate : MonoBehaviour
+{
+    public float rotateSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
